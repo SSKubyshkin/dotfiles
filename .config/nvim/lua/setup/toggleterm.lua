7e2d253cca3555e87ca87fa3ce07dffab3ffdc30
Sergey Kubyshkin <sskubyshkin@gmail.com>
2022-12-11 09:37:02 +1200

    nvim: Update toggleterm configuration

--- .config/nvim/lua/setup/toggleterm.lua
+++ .config/nvim/lua/setup/toggleterm.lua
@@ -1,3 +1,10 @@
 require("toggleterm").setup {
-    open_mapping = [[<A-\>]]
+    open_mapping = [[<A-\>]],
+    direction = 'float',
+    float_opts = {
+	border = 'single',
+	width = 120,
+	height = 25,
+	winblend = 3,
+    },
 }
